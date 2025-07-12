290b4ee55d2723b62f85f747665f1f79330545c9
Gaston <gaston.aveline@gmail.com>
2025-07-12 14:02:31 +0200

    finish removing old files and directories

--- fix_scored_jobs.py
+++
@@ -1,8 +0,0 @@
-# %%
-import pandas as pd
-
-df=pd.read_pickle('scored_jobs.pkl')
-df.rename(columns={'match_pct':'AI_score'},inplace=True)
-df.AI_score=(df.AI_score//10).astype(int)
-# %%
-df.to_pickle("scored_jobs.pkl")
