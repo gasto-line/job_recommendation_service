2b1f1475e3aaad8de6ff339a4d63b4bba30f5714
Gaston <gaston.aveline@gmail.com>
2025-11-18 13:57:21 +0100

    create a test.sh file to troubleshoot the github action failure

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+aws ec2 run-instances \
+  --image-id ami-0c814f1cf8f298648 \
+  --count 1 \
+  --instance-type t3.micro \
+  --associate-public-ip-address \
+  --key-name my-debug-key \
+  --iam-instance-profile Name=EC2-get-model-role \
+  --user-data file://inference_VM/user-data-test.sh \
+  --security-group-ids 	sg-0ccbd2405bff7b168 \
+  --region eu-west-3 \
+  --query 'Instances[0].InstanceId' \
+  --output text
