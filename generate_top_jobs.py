0a240f9cc0685e2759a66ab63cd1039290cbb1f2
Gaston Aveline <110301650+gasto-line@users.noreply.github.com>
2025-11-20 15:33:35 +0100

    Dev (#47)
    * update the CRON job to trigger at 8A.M.
    
    * centralize DB jobs in one file
    
    * Add error handling on DB insertion
    
    * Quick fix on connection string
    
    * New bash scripts launching ec2 instance on aws that download the french model of fasttext from huggingface and expose an endpoint for inference accessible from my private laptop only
    
    * Automatic production of embeddings for job listing and a single embedding representing the user's ideal job
    
    * Add a new fasttext scoring value in the dataframe
    
    * edit requirements.txt
    
    * remove the deprecated email_sending procedure and update requirements.txt
    
    * remove the email_sending procedure for real
    
    * Added the AWS credential config to the workflow
    
    * edit the worlflow file to give token write permissions
    
    * add aws cli installation process in the workflow
    
    * create a test.sh file to troubleshoot the github action failure
    
    * edit the workflow title
    
    * edit workflow file to verify the VM is launched on AWS
    
    * make a test to verify it's indeed the subprocess that is causing problem
    
    * same type of test
    
    * try to unbuffer python to see logs
    
    * change SG for testing the workflow
    
    * small change
    
    * produce fasttext_toplist.pkl file
    
    * add .pkl generation script in workflow for fasttext
    
    * merge and commit
    
    * clean merge residuals

--- generate_top_jobs.py
+++ generate_top_jobs.py
@@ -62,11 +62,17 @@ jobs_title_scores = get_field_wise_scoring(jobs_description_grouped_embeddings,"
 jobs_general_scores=np.mean([jobs_description_scores]+[jobs_title_scores],axis=0)
 filtered_df["fasttext_score"]=jobs_general_scores
 <<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> main
 fasttext_toplist = filtered_df.sort_values("fasttext_score", ascending=False).head(TOP_N)
 FASTTEXT_PICKLE= "fasttext_toplist.pkl"
 fasttext_toplist.to_pickle(FASTTEXT_PICKLE)
 print(f"Top fasttext {TOP_N} jobs saved to {FASTTEXT_PICKLE}")
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> main
 >>>>>>> main
 
 #%%
@@ -83,9 +89,12 @@ print(f"Top {TOP_N} jobs saved to {OUTPUT_PICKLE}")
 
 <<<<<<< HEAD
 =======
+<<<<<<< HEAD
+=======
 
 
 
+>>>>>>> main
 >>>>>>> main
 """
 from email_sending import send_email
