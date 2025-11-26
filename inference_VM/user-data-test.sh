e185610c5fc18da29e2cfe7de17ac45cc33d40e1
Gaston <gaston.aveline@gmail.com>
2025-11-26 12:36:07 +0100

    Add an error handling script for the api call and reduce the batch size to 20 lines

--- inference_VM/user-data-test.sh
+++ inference_VM/user-data-test.sh
@@ -20,7 +20,7 @@ dnf install -y python3-pip awscli gcc gcc-c++ make git python3-devel
 # --- Install Python dependencies ---
 #python3 -m venv /home/ec2-user/venv
 # /home/ec2-user/venv/bin/activate
-python3 -m pip install --upgrade --ignore-installed "numpy<2.0.0" setuptools wheel boto3 fasttext fastapi uvicorn
+python3 -m pip install --upgrade --ignore-installed "numpy<2.0.0" setuptools wheel boto3 fasttext fastapi uvicorn psutil
 
 # create the worker script for inference and language identification
 cat << EOF > worker.py
@@ -56,7 +56,7 @@ EOF
 # create the inference srcipt to run
 cat << EOF > app.py
 import boto3
-import subprocess, json, os, time
+import subprocess, json, os, time, psutil
 from typing import List
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
@@ -99,16 +99,24 @@ def call_worker(model_path, input_path):
         raise RuntimeError(f"Worker failed: {proc.stderr}")
     return json.loads(proc.stdout)
 
+def print_memory(label):
+    process = psutil.Process(os.getpid())
+    mem_mb = process.memory_info().rss / (1024*1024)
+    print(f"[RAM] {label}: {mem_mb:.2f} MB")
+
 # Application endpoint
 @app.post("/embed")
 def get_embedding(data: TextInput):
+
+    print_memory("after calling enpoint, before loading input")
     input = data.input
+    print_memory("after loading input")
 
     print("Writing the input tokens to disk")
     with open(input_path,"w") as f:
         json.dump(input, f)
-
     print("File size:", os.path.getsize(input_path))
+    print_memory("after writing input to disk")
 
     # Run the language sorting worker leveraging the fasttext language detection model
     # Returns a dictionnary having one key for each language FR & EN 
@@ -121,6 +129,7 @@ def get_embedding(data: TextInput):
     except RuntimeError as e:
         raise HTTPException(status_code=500, detail=f"worker failed on language identification: {e}")
     print("Retrieving worker output")
+    print_memory("after getting grouped input from language identification worker")
 
     print("Writing french inference model input to disk")
     with open(fr_embeddings_path,"w") as f:
@@ -130,6 +139,8 @@ def get_embedding(data: TextInput):
     with open(en_embeddings_path,"w") as f:
         json.dump(group_input["EN"][1],f)
 
+    print_memory("after writing the grouped input to disk")
+
     # With the output grouped by language key we can run the inference in batches
     # The inference is applied running a subprocess on the second list
     print("Calling worker for french model inference")
@@ -138,19 +149,21 @@ def get_embedding(data: TextInput):
     except RuntimeError as e:
         raise HTTPException(status_code=500, detail=f"worker failed on french model inference: {e}")
     print("French model inference retrieved")
+    print_memory("after getting the french model output")
 
     print("Calling worker for english model inference")
-    EN_input = group_input["EN"][1]
     try:
         EN_output = call_worker(model_en_path,en_embeddings_path)["embeddings"]
     except RuntimeError as e:
         raise HTTPException(status_code=500, detail=f"worker failed on english model inference: {e}")
     print("English model inference retrieved")
+    print_memory("after getting the english model output")
 
     # Create a output variable
     group_output=group_input.copy()
     group_output["FR"][1]=FR_output
     group_output["EN"][1]=EN_output
+    print_memory("after merging for final output")
 
     return (group_output)
     
