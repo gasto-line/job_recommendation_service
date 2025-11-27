ac64b5b31ba0c6c6d601321fc879bea4b7332ec8
Gaston <gaston.aveline@gmail.com>
2025-11-27 18:54:49 +0100

    small fixes

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import re
 
 # Function that generates a unique identifier for the jobs
 def generate_job_hash(title, company, pub_date):
@@ -6,3 +7,16 @@ def generate_job_hash(title, company, pub_date):
     month = pub_date.month
     identifier = f"{title.strip().lower()}_{company.strip().lower()}_{year}_{month}"
     return pd.util.hash_pandas_object(pd.Series(identifier)).astype(str)[0]
+
+
+def tokenization(text):
+    text = re.sub(r"[^a-zA-Z0-9\s#&œ+éèêàâç'’]","",text)
+    text = text.lower()
+    text = re.split(r"['’\s]",text)
+    while "" in text:
+        text.remove("")
+    while "l" in text:
+        text.remove("l")
+    while "d" in text:
+        text.remove("d")
+    return (text)
