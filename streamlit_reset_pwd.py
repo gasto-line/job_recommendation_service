2cebdc9d928a2e79d81a82cc038b6d19a49a6e0b
Gaston <gaston.aveline@gmail.com>
2025-12-16 11:39:46 +0100

    add the code to create a client in the reset password link

--- streamlit_reset_pwd.py
+++ streamlit_reset_pwd.py
@@ -1,6 +1,12 @@
 import streamlit as st
-import supabase
+from supabase import create_client
 
+SUPABASE_URL = st.secrets["SUPABASE_URL"]
+SUPABASE_KEY = st.secrets["SUPABASE_ANON_KEY"]
+supabase = create_client(
+            SUPABASE_URL,
+            SUPABASE_KEY
+        )
 
 st.subheader("Reset your password")
 
