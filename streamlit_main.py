ecac26745b379b8dd0938e5312a0819aa430cf59
Gaston <gaston.aveline@gmail.com>
2025-12-16 10:32:16 +0100

    same

--- streamlit_main.py
+++ streamlit_main.py
@@ -377,7 +377,7 @@ def profile_page():
 # MAIN NAVIGATION
 # ---------------------------------------------------------
 def main():
-    st.info(st.query_params.type)
+    st.info(st.query_params.get_all())
     if st.query_params.get_all("type")=="recovery":
         st.subheader("Reset your password")
 
