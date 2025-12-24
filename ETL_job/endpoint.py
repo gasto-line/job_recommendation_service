5b5b252fcb4cb4d2d94fb96649dd6b8dd5c6dc51
Gaston <gaston.aveline@gmail.com>
2025-12-24 10:21:15 +0100

    edit the path to the launch VM script and add chmod permission in the user data to execute it

--- ETL_job/endpoint.py
+++ ETL_job/endpoint.py
@@ -3,10 +3,16 @@ from ideal_job_embedding_generator import generate_ideal_jobs
 from launch_VM import launch_inference_instance
 from fasttext_process import call_api
 from DB_jobs import insert_embeddings
+from pathlib import Path
 import numpy as np
 import boto3
 
 app = FastAPI()
+
+# This file is inside: job_recommendation_service/ETL_job/...
+REPO_ROOT = Path(__file__).resolve().parents[1]
+
+VM_SCRIPT_PATH = REPO_ROOT / "workflow_VM" / "VM_config.sh"
 
 @app.get("/health")
 def health():
@@ -20,13 +26,15 @@ def generate_ideal_job_embeddings(user_profile: dict = Body(...)):
         ideal_jobs = generate_ideal_jobs(user_profile)
 
         # Launch inference VM
+        # It returns a dict in format: {"title": ["title1_fr", "title1_en" ,... ,"title3_en"], "description":["description1_fr", "description1_en" ,... ,"description3_en"]}
         public_ip, instance_id = launch_inference_instance(
-            "workflow_VM/VM_config.sh",
+            str(VM_SCRIPT_PATH),
             instance_type="t3.large")
 
         # Call inference API
         ideal_jobs_embeddings = {}
         for field in ideal_jobs.keys():
+            # The inference VM automatically sorts the list of field values by language
             field_grouped_embeddings=call_api(public_ip, ideal_jobs[field], "sentence")
             for lang in field_grouped_embeddings.keys():
                 ideal_jobs_embeddings[lang]=np.mean(field_grouped_embeddings[lang][1], axis=0)
